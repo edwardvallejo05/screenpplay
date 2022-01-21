a965f8844ac390addee9517e20d05fd2e22d5fe1
Edward Vallejo <edward.vallejo@wiedii.co>
2022-01-20 22:26:26 -0500

    Run a firts Test Sauce Demo with locators

--- src/test/java/starter/navigation/theSauceDemoGoHomePage.java
+++
@@ -1,8 +0,0 @@
-package starter.navigation;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://www.saucedemo.com/")
-public class theSauceDemoGoHomePage extends PageObject {
-}
